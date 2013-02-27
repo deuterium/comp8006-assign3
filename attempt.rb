9270abae71c87dbaaf7363ce653d1dd15ac2768a
Chris Wood <chriswood.ca@gmail.com>
2013-02-26 20:49:00 -0800

    Attempt object added

---
+++ attempt.rb
@@ -0,0 +1,21 @@
+class Attempt
+	def initialize(month, day, time, ip)
+	  @month	= month
+	  @day		= day
+	  @time		= time
+	  @ip		= ip
+	end
+	def month
+	  @month
+	end
+	def day
+	  @day
+	end
+	def time
+	  @time
+	end
+	def ip
+	  @ip
+	end
+end
+
