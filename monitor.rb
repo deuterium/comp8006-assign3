4d01d15e2aae1bd2913bdc3a43532e90f8061da0
Chris Wood <chriswood.ca@gmail.com>
2013-02-26 20:47:24 -0800

    initial parse line functionallity Line to parse for lines containing "Failed password" Lines containing this have month, day, time and ip created to object to then stored to global array

--- monitor.rb
+++ monitor.rb
@@ -1,12 +1,24 @@
 #!/usr/bin/ruby
 #
+load "attempt.rb"
+
 puts "This is a test ruby script: " + __FILE__
+
+#Parse line for authentication
+def parseLine(line)
+	if line.include? "Failed password"
+		puts line
+		lineArray = line.split(pattern=" ")
+		o = Attempt.new(lineArray[0], lineArray[1], lineArray[2], lineArray[10])
+		puts o.ip
+	end
+end
 
 #open file
 begin
 	file = File.new(ARGV[0], "r")
 	while (line = file.gets)
-		puts line
+		parseLine(line)
 	end
 	file.close
 rescue => err
