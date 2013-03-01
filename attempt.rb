00918248f89e12f042d6bbe18e094379efb1b7f8
Chris Wood <chriswood.ca@gmail.com>
2013-02-28 20:17:25 -0800

    Moved methods relating to object data in to the object Fixed parse to include lines where authentication fails for unknown users..... thanks someone probing my system while I was asleep for helping me notice this.

--- attempt.rb
+++ attempt.rb
@@ -18,5 +18,27 @@ class Attempt
 	def ip
 	  @ip
 	end
+	
+	#Converts shorthand month strings to numerical numbers
+	def monthToDig(month)
+        	case month
+        	when "Jan"
+                	return 1
+        	when "Feb"
+                	return 2
+        	when "Mar"
+                	return 3
+        	when "Apr"
+                	return 4
+	        end
+        	#etc.....
+        end
+        
+	#Returns a time object constructed from an attempt object's local variables
+	def to_time
+        	hms = @time.split(pattern=":")
+        	t = Time.new(Time.now.year, monthToDig(@month), @day.to_i, hms[0].to_i, hms[1].to_i, hms[2].to_i)
+        	return t
+	end
 end
 
--- monitor.rb
+++ monitor.rb
@@ -25,9 +25,12 @@ $attempts = []
 
 #Parse line for authentication
 def parseLine(line)
-	if line.include? "Failed password"
+	if line.include? "Failed password" and !line.include? "invalid user"
 		lineArray = line.split(pattern=" ")
-		$attempts.push(Attempt.new(lineArray[0], lineArray[1], lineArray[2], lineArray[10]))
+		$attempts.push(Attempt.new(lineArray[0], lineArray[1], lineArray[2], lineArray[10]))	
+	elsif line.include? "Failed password" and line.include? "invalid user"
+		lineArray = line.split(pattern=" ")
+		$attempts.push(Attempt.new(lineArray[0], lineArray[1], lineArray[2], lineArray[12]))
 	end
 end
 
@@ -45,29 +48,9 @@ def checkAttempts()
 	sameAttempts.each do |a|
 		#compare times for stuff here, if close, new array? :)
 		#new array length compared to numOfAttempts and then iptables rule
+		puts a.to_time
+		print a.ip + ": "
 	end
-end
-
-#Converts shorthand month strings to numerical numbers
-def monthToDig(month)
-	case month
-	when "Jan"
-		return 1
-	when "Feb"
-		return 2
-	when "Mar"
-		return 3
-	when "Apr"
-		return 4
-	end
-	#etc.....
-end
-
-#Returns a time object constructed from an attempt object's local variables
-def toTime(a)
-	hms = a.time.split(pattern=":")
-	time = Time.new(Time.now.year, monthToDig(a.month), a.day.to_i, hms[0].to_i, hms[1].to_i, hms[2].to_i)
-	return time
 end
 
 ##BEGIN LOGIC
